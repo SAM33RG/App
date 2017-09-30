2b390daf4b50f025e68bb99a9fe3bcc8cf384bf2
SAM33RG <sameergangar1@gmail.com>
2017-09-30 13:36:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sam33r/app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sam33r.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG ="MEASSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
